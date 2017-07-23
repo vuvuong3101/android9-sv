9186833505db9c4f5401d026a90c34722d94443f
vuvuong3101 <boybg1996@gmail.com>
2017-07-24 03:34:15 +0700

    up
    up

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+var Hottie = require('hoties');
 
+mongoose.connect('mongodb://<admin>:<admin>@ds049651.mlab.com:49651/serverapp',{useMongoClient: true});
+////
+
+var hottie = new Hottie({
+  name : "LinkKA",
+  age : 19,
+  image : "http://asssddad.net"
+});
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -14,7 +24,7 @@ app.get('/', function(request, response) {
 });
 app.get('/api', function(req, res) {} );
   res.json({'hello' :'world'});
-);
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
